7f22c530497a4cd305cc891b95bbc3ea1e680ef7
Dennis <d7chau@gmail.com>
2021-08-29 17:02:58 -0400

    Added ability to solve basic single digit calculations

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var operatorNumberArray = [];
+
 var oneBtn = document.getElementById("oneBtn");
 var twoBtn = document.getElementById("twoBtn");
 var threeBtn = document.getElementById("threeBtn");
@@ -8,7 +10,7 @@ var sevenBtn = document.getElementById("sevenBtn");
 var eightBtn = document.getElementById("eightBtn");
 var nineBtn = document.getElementById("nineBtn");
 var acBtn = document.getElementById("acBtn");
-var cBtn = document.getElementById("cBtn");
+var plusminusBtn = document.getElementById("plusMinusBtn");
 var remainderBtn = document.getElementById("remainderBtn");
 var addBtn = document.getElementById("addBtn");
 var subtractBtn = document.getElementById("subtractBtn");
@@ -18,23 +20,25 @@ var decimalBtn = document.getElementById("decimalBtn");
 var equalBtn = document.getElementById("equalBtn");
 var displayText = document.getElementById("displayText");
 
-zeroBtn.addEventListener("click", () => handleDisplay(0));
-oneBtn.addEventListener("click", () => handleDisplay(1));
-twoBtn.addEventListener("click", () => handleDisplay(2));
-threeBtn.addEventListener("click", () => handleDisplay(3));
-fourBtn.addEventListener("click", () => handleDisplay(4));
-fiveBtn.addEventListener("click", () => handleDisplay(5));
-sixBtn.addEventListener("click", () => handleDisplay(6));
-sevenBtn.addEventListener("click", () => handleDisplay(7));
-eightBtn.addEventListener("click", () => handleDisplay(8));
-nineBtn.addEventListener("click", () => handleDisplay(9));
-remainderBtn.addEventListener("click", () => handleDisplay("%"));
-addBtn.addEventListener("click", () => handleDisplay("+"));
-subtractBtn.addEventListener("click", () => handleDisplay("-"));
-multiplyBtn.addEventListener("click", () => handleDisplay("*"));
-divideBtn.addEventListener("click", () => handleDisplay("/"));
-decimalBtn.addEventListener("click", () => handleDisplay("."));
-equalBtn.addEventListener("click", () => handleDisplay("="));
+zeroBtn.addEventListener("click", () => changeDisplay(0));
+oneBtn.addEventListener("click", () => changeDisplay(1));
+twoBtn.addEventListener("click", () => changeDisplay(2));
+threeBtn.addEventListener("click", () => changeDisplay(3));
+fourBtn.addEventListener("click", () => changeDisplay(4));
+fiveBtn.addEventListener("click", () => changeDisplay(5));
+sixBtn.addEventListener("click", () => changeDisplay(6));
+sevenBtn.addEventListener("click", () => changeDisplay(7));
+eightBtn.addEventListener("click", () => changeDisplay(8));
+nineBtn.addEventListener("click", () => changeDisplay(9));
+remainderBtn.addEventListener("click", () => changeDisplay("%"));
+addBtn.addEventListener("click", () => changeDisplay("+"));
+subtractBtn.addEventListener("click", () => changeDisplay("-"));
+multiplyBtn.addEventListener("click", () => changeDisplay("x"));
+divideBtn.addEventListener("click", () => changeDisplay("/"));
+decimalBtn.addEventListener("click", () => changeDisplay("."));
+equalBtn.addEventListener("click", () => computeAnswer());
+acBtn.addEventListener("click", () => changeDisplay(""));
+// plusMinusBtn.addEventListener("click", () => changeDisplay(""));
 
 function add(a, b) {
   return a + b;
@@ -54,18 +58,32 @@ function divide(a, b) {
 
 function operate(operator, num1, num2) {
   if (operator === "+") {
-    add(num1, num2);
+    return add(num1, num2);
   } else if (operator === "-") {
-    subtract(num1, num2);
+    return subtract(num1, num2);
   } else if (operator === "*") {
-    multiply(num1, num2);
+    return multiply(num1, num2);
   } else {
-    divide(num1, num2);
+    return divide(num1, num2);
   }
 }
 
-function handleDisplay(text) {
+function changeDisplay(text) {
   displayText.textContent = text;
+  operatorNumberArray.push(text);
+}
+
+function computeAnswer() {
+  var result = operatorNumberArray[0];
+
+  for (i = 1; i < operatorNumberArray.length; i += 2) {
+    operator = operatorNumberArray[i];
+    number = operatorNumberArray[i + 1];
+
+    result = operate(operator, result, number);
+  }
+  displayText.textContent = result;
+  operatorNumberArray = [];
 }
 
 // operate("+", 1, 4);
